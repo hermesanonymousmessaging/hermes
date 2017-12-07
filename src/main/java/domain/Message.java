ce4091ffc620c11f0ff9c61fc52fdcd671d97813
kgn95 <kaganozgun@gmail.com>
2017-12-08 01:02:38 +0300

    GetTimeString added to Message class

--- src/main/java/domain/Message.java
+++ src/main/java/domain/Message.java
@@ -49,8 +49,34 @@ public class Message {
 		this.id = id;
 	}
 
-	public Date getTimestamp() {
+	public Date getTimestampD() {
 		return timestamp;
+	}
+	public String getTimestamp() {
+		String hour,min,day,month;
+		if(timestamp.getDate() < 10) {
+			day = "0"+Integer.toString(timestamp.getDate());
+		}else {
+			day = Integer.toString(timestamp.getDate());
+		}
+		if(timestamp.getHours() < 10) {
+			hour = "0"+Integer.toString(timestamp.getHours());
+		}else {
+			hour = Integer.toString(timestamp.getHours());
+		}
+		if(timestamp.getMinutes() < 10) {
+			min = "0"+Integer.toString(timestamp.getMinutes());
+		}else {
+			min = Integer.toString(timestamp.getMinutes());
+		}
+		if(timestamp.getMonth() < 10) {
+			month = "0"+Integer.toString(timestamp.getMonth());
+		}else {
+			month = Integer.toString(timestamp.getMonth());
+		}
+		String year = Integer.toString(timestamp.getYear()+1900);
+		String date = hour + ":" + min + "  " + day + "/" + month + "/" + year;
+		return date;
 	}
 
 	public void setTimestamp(Date timestamp) {
