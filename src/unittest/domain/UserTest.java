24acc66c796183367ac8113b6e1bd3c0b7abf419
berk <bdehrioglu@yahoo.com>
2017-12-10 02:45:24 +0300

    coverage is increased for user unit tests

--- src/unittest/domain/UserTest.java
+++ src/unittest/domain/UserTest.java
@@ -27,24 +27,36 @@ public class UserTest {
     public ExpectedException exception = ExpectedException.none();
 	
 	private static User Usertest;
+	private static User Usertest2;
+	private static User Usertest3;
     private static String username = "bedri";
     private static String firstName= "berk";
     private static String lastName= "dehri";
     private static String password= "150694"; //CHANGE THIS 
     private static String email= "bdehri@yahoo.com";
     private static String phoneNumber= "00905376525265";
+    private static String photoUrl = "url";
+    private static List<String> list ;
+
 
     
 	@BeforeClass
     public static void setUpBeforeClass() throws ParseException {
 		Usertest = new User();
+		Usertest.setId("123");
 		Usertest.setUsername(username);
 		Usertest.setFirstName(firstName);
 		Usertest.setLastName(lastName);
 		Usertest.setEmail(email);
 		Usertest.setPhoneNumber(phoneNumber);
 		Usertest.setPassword(password);
-    }
+		Usertest.addChannel("123");
+		Usertest.addChannel("124");
+		Usertest.setProfilePicture(photoUrl);
+		Usertest.removeChannel("123");
+		Usertest2 = new User(firstName,lastName);
+		Usertest3 = new User(firstName,lastName, email, password, username, phoneNumber,photoUrl);		
+	}
 	
 	
 	@Test
@@ -76,5 +88,35 @@ public class UserTest {
 	public void testgetPassword() throws ParseException {
 		Assert.assertEquals(password,Usertest.getPassword());
 	}
+	
+	@Test
+	public void testgetId() throws ParseException {
+		Assert.assertEquals("123",Usertest.getId());
+	}
+	
+	@Test
+	public void testgetFirstname2() throws ParseException {
+		Assert.assertEquals(firstName,Usertest2.getFirstName());
+	}
+	
+	@Test
+	public void testgetLastname2() throws ParseException {
+		Assert.assertEquals(lastName,Usertest2.getLastName());
+	}
+	
+	
+	@Test
+	public void testConstructor() throws ParseException{
+		Assert.assertEquals(firstName,Usertest3.getFirstName());
+		Assert.assertEquals(lastName,Usertest3.getLastName());
+		Assert.assertEquals(username,Usertest3.getUsername());
+		Assert.assertEquals(password,Usertest3.getPassword());
+		Assert.assertEquals(phoneNumber,Usertest3.getPhoneNumber());
+		Assert.assertEquals(photoUrl,Usertest3.getProfilePicture());
+	}
+	
+	
+	
+
 	
 }
