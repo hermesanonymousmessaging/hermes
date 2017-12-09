f5a1765a64df64c851012e9cf1d12ca69f5d0a6c
birkandenizer <denizerb@itu.edu.tr>
2017-12-09 23:33:16 +0300

    Added unit test for Log class

--- src/main/java/domain/Log.java
+++ src/main/java/domain/Log.java
@@ -36,36 +36,10 @@ public class Log {
 		this.id = id;
 	}
 
-	public Date getTimestampD() {
+	public Date getTimestamp() {
 		return timestamp;
 	}
-	public String getTimestamp() {
-		String hour,min,day,month;
-		if(timestamp.getDate() < 10) {
-			day = "0"+Integer.toString(timestamp.getDate());
-		}else {
-			day = Integer.toString(timestamp.getDate());
-		}
-		if(timestamp.getHours() < 10) {
-			hour = "0"+Integer.toString(timestamp.getHours());
-		}else {
-			hour = Integer.toString(timestamp.getHours());
-		}
-		if(timestamp.getMinutes() < 10) {
-			min = "0"+Integer.toString(timestamp.getMinutes());
-		}else {
-			min = Integer.toString(timestamp.getMinutes());
-		}
-		if(timestamp.getMonth() < 10) {
-			month = "0"+Integer.toString(timestamp.getMonth());
-		}else {
-			month = Integer.toString(timestamp.getMonth());
-		}
-		String year = Integer.toString(timestamp.getYear()+1900);
-		String date = hour + ":" + min + "  " + day + "/" + month + "/" + year;
-		return date;
-	}
-
+	
 	public void setTimestamp(Date timestamp) {
 		this.timestamp = timestamp;
 	}
---
+++ src/unittest/domain/LogTest.java
@@ -0,0 +1,50 @@
+package domain;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+
+import domain.Log;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class LogTest {
+	@Rule
+    public ExpectedException exception = ExpectedException.none();
+	
+	private static Log testLog;
+	private static String id = "0123456789";
+	private static Date timestamp = new Date();
+	private static String text = "This is a test log";
+	
+	@BeforeClass
+    public static void setUpBeforeClass() {
+        testLog = new Log();
+        testLog.setId(id);
+        testLog.setTimestamp(timestamp);
+        testLog.setText(text);
+    }
+	
+	@Test
+	public void testgetId() {
+		Assert.assertEquals(id,testLog.getId());
+	}
+	
+	@Test
+	public void testgetTimestamp() {
+		Assert.assertEquals(timestamp,testLog.getTimestamp());
+	}
+	
+	@Test
+	public void testgetText() {
+		Assert.assertEquals(text,testLog.getText());
+	}
+	
+}
