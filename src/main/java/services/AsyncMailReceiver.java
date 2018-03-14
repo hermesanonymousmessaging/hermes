c84a2f571ca2d7e342f66d4dfb52a9b584068098
Berkay Giriş <berkaygiris@gmail.com>
2018-03-14 16:53:58 +0300

    Parsed received mails

--- src/main/java/services/AsyncMailReceiver.java
+++ src/main/java/services/AsyncMailReceiver.java
@@ -1,7 +1,10 @@
 package services;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
+import javax.mail.BodyPart;
 import javax.mail.Flags;
 import javax.mail.Folder;
 import javax.mail.Message;
@@ -9,9 +12,12 @@ import javax.mail.MessagingException;
 import javax.mail.NoSuchProviderException;
 import javax.mail.Session;
 import javax.mail.Store;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.mail.search.FlagTerm;
 
+import org.jsoup.Jsoup;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -50,9 +56,9 @@ public class AsyncMailReceiver {
 	           Message message = messages[i];
 	           System.out.println("---------------------------------");
 	           System.out.println("Email Number " + (i + 1));
-	           System.out.println("Subject: " + message.getSubject());
+	           System.out.println("Subject: " + message.getSubject().replace("Re: ","").replace("Re:",""));
 	           System.out.println("From: " + message.getFrom()[0]);
-	           System.out.println("Text: " + message.getContent().toString());
+	           System.out.println("Text: " + getTextFromMessage(message));
 	           MimeMessage source = (MimeMessage) message;
 	           MimeMessage copy = new MimeMessage(source);
 	        }
@@ -67,6 +73,40 @@ public class AsyncMailReceiver {
 	        } catch (Exception e) {
 	           e.printStackTrace();
 	        }
+	}
+	
+	private String getTextFromMessage(Message message) 
+			throws MessagingException, IOException {
+	    String result = "";
+	    if (message.isMimeType("text/plain")) {
+	        result = message.getContent().toString();
+	    } else if (message.isMimeType("multipart/*")) {
+	        MimeMultipart mimeMultipart = (MimeMultipart) message.getContent();
+	        result = getTextFromMimeMultipart(mimeMultipart);
+	    }
+	    String lines[] = result.split("\\r?\\n");
+        if(lines.length >= 1)
+        	result = lines[1];
+	    return result;
+	}
+
+	private String getTextFromMimeMultipart(MimeMultipart mimeMultipart)  
+			throws MessagingException, IOException{
+	    String result = "";
+	    int count = mimeMultipart.getCount();
+	    for (int i = 0; i < count; i++) {
+	        BodyPart bodyPart = mimeMultipart.getBodyPart(i);
+	        if (bodyPart.isMimeType("text/plain")) {
+	            result = result + "\n" + bodyPart.getContent();
+	            break; // without break same text appears twice in my tests
+	        } else if (bodyPart.isMimeType("text/html")) {
+	            String html = (String) bodyPart.getContent();
+	            result = result + "\n" + Jsoup.parse(html).text();
+	        } else if (bodyPart.getContent() instanceof MimeMultipart){
+	            result = result + getTextFromMimeMultipart((MimeMultipart)bodyPart.getContent());
+	        }
+	    }
+	    return result;
 	}
 	
 }
